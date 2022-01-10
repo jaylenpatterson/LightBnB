826eeadcdf39bf88a59fd6270eda2900cf90b852
jaylenpatterson <jaylenpatterson16@gmail.com>
2022-01-10 02:59:18 +0000

    refactored getAllReservations function

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -96,7 +96,26 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const query = {
+  text:`SELECT reservations.*, properties.*, avg(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id 
+    WHERE property_reviews.guest_id = $1
+    AND reservations.end_date < now()::date
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2`,
+  values: [guest_id, limit]
+}
+  return pool
+    .query(query)
+    .then((result) => {
+      return result.rows
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.getAllReservations = getAllReservations;
 
@@ -133,3 +152,21 @@ const addProperty = function(property) {
   return Promise.resolve(property);
 }
 exports.addProperty = addProperty;
+
+
+// const query = {
+  //   text:`SELECT * 
+  //   FROM properties 
+  //   WHERE guest_id = $1
+  //   limit $2;`,
+  //   values: [guest_id, limit]
+  // };
+
+  // return pool
+  //   .query(query)
+  //   .then((result) => {
+  //    return result.rows
+  //   })
+  //   .catch((err) => {
+  //     console.log(err.message);
+  //   });
